eb87b3724a9dd55d15dd7f34f8e88814b9a97c13
Zarar Khan <zararkkhan@gmail.com>
2019-04-10 16:26:29 -0400

    Initial commit

---
+++ Apple picker/Assets/Basket.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour {
+
+
+    public Text scoreGT;
+
+	// Use this for initialization
+	void Start () {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text> ();
+        scoreGT.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Gets the current position of the mouse
+		Vector3 mousePos2D = Input.mousePosition;
+
+		// Sets how far to push the mouse into 3D
+		mousePos2D.z = -Camera.main.transform.position.z;
+
+		// Convert the point from 2D scren space into 3D game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+
+		// Move the basket based on the mouse
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos; // move the basket
+		
+	}
+
+	void OnCollisionEnter(Collision coll){
+		// something hit the basket what was it?
+		GameObject collidedWith = coll.gameObject;
+		// if it was an Apple destroy it
+		if (collidedWith.tag == "Apple")
+			Destroy (collidedWith);
+
+        int score = int.Parse(scoreGT.text);
+        score += 100;
+        scoreGT.text = score.ToString();
+
+	}
+}
---
+++ Assets/AppleTree.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+	public GameObject applePrefab;
+	public float speed = 50f; // speed of Apple Tree
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirections = 0.1f;
+	public float secondsBetweenAppleDrops = 0.00001f;
+
+	// Use this for initialization
+	void Start () {
+		// wait 2 seconds before dropping an apple
+		Invoke ("DropApple", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Basic Movement
+		// Get current position of AppleTree
+		Vector3 pos = transform.position;
+		// Calculate the amount of movement
+		pos.x += speed * Time.deltaTime;
+		// Move the tree by pos distance
+		transform.position = pos;
+		// Change Direction
+		// if we hit the left edge
+		if (pos.x < -leftAndRightEdge) {
+			// change direction
+			speed = Mathf.Abs (speed);
+		}
+		// if we hit the left edge
+		else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed);
+		// every 10% of the time change direction randomly
+		} else if (Random.value < chanceToChangeDirections) {
+			speed *= -1; 
+		}
+	}
+
+	void DropApple(){
+		// create an apple
+		GameObject apple = Instantiate<GameObject> (applePrefab);
+		// Put the apple in the tree
+		apple.transform.position = transform.position;
+		// Repeat every few seconds
+		Invoke ("DropApple", secondsBetweenAppleDrops);
+	}
+}
